8c105a4fbbc189da8edb61c7f7c2d9b04558f668
yeji-gim <77161592+yeji-gim@users.noreply.github.com>
2023-02-19 12:17:42 +0900

    Add files via upload

---
+++ Scripts/UI/Popup/UI_Popup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+   
+public class UI_Popup : UI_Base
+{
+    public override void Init()
+    {
+        Manager.UI.SetCanvas(gameObject, true);
+    }
+
+    public virtual void ClosePopupUI()
+    {
+        Manager.UI.ClosePopupUI(this);
+    }
+
+}
+
